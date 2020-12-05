9cd4d46dce81ef4530723b0a20ea61cfe0c70f7f
sunh5 <sunh5@rose-hulman.edu>
2020-12-05 01:07:01 -0500

    Initial Commit

---
+++ Flexible Layout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flexible Layout
+//
+//  Created by Haoxuan Sun on 12/5/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
